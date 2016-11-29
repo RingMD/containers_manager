10bf8ebaa046b6c2391bace45c798e21bfb9ac3f
Duc Le <leminhducktvn@gmail.com>
2016-11-29 22:29:38 +0800

    Check the configuration

--- lib/containers_manager/cli.rb
+++ lib/containers_manager/cli.rb
@@ -6,7 +6,22 @@ module ContainersManager
 
     desc 'exec', 'Execute jobs to manage containers'
     def exec
-      Application.exec
+      exception_rescue do
+        Application.exec
+      end
+    end
+
+    desc 'check', 'Output the configuration'
+    def exec
+      exception_rescue do
+        Application.check
+      end
+    end
+
+    private
+
+    def exception_rescue
+      yield
     rescue Exception => e
       $stdout.puts(e.message)
     end
